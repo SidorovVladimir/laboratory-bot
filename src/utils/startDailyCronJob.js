177f5dc3c26f329844130e740537965e93608d3f
Sidorov Vladimir <v.sidorov29091988@gmail.com>
2026-02-17 09:33:17 +0700

    refactor startDailyCronJob function

--- src/utils/startDailyCronJob.js
+++ src/utils/startDailyCronJob.js
@@ -1,6 +1,5 @@
 import cron from 'node-cron';
 import { client } from '../db.js';
-import { messages } from '@electric-sql/pglite';
 
 export const startDailyCronJob = (bot) => {
   cron.schedule('0 0 9 * * *', async () => {
@@ -8,42 +7,42 @@ export const startDailyCronJob = (bot) => {
 
     try {
       const briefingLog = await client.query(
-        `SELECT full_name FROM briefing_log WHERE "date" + (periodicity * INTERVAL '1 month') < CURRENT_DATE + 5`
+        `SELECT full_name FROM briefing_log WHERE "date" + (periodicity * INTERVAL '1 month') <= CURRENT_DATE + 5`
       );
       const fireBriefingLog = await client.query(
-        `SELECT full_name FROM fire_briefing_log WHERE "date" + (periodicity * INTERVAL '1 month') < CURRENT_DATE + 5`
+        `SELECT full_name FROM fire_briefing_log WHERE "date" + (periodicity * INTERVAL '1 month') <= CURRENT_DATE + 5`
       );
       const powerToolLog = await client.query(
-        `SELECT name FROM power_tool_inspection_log WHERE "date" + (periodicity * INTERVAL '1 month') < CURRENT_DATE + 5`
+        `SELECT name, inventory_number FROM power_tool_inspection_log WHERE "date" + (periodicity * INTERVAL '1 month') <= CURRENT_DATE + 5`
       );
       const slingLog = await client.query(
-        `SELECT name FROM sling_inspection_log WHERE "date" + INTERVAL '10 days' < CURRENT_DATE + 5`
+        `SELECT name, inventory_number FROM sling_inspection_log WHERE "date" + INTERVAL '10 days' <= CURRENT_DATE + 5`
       );
 
       const alerts = [];
 
       if (briefingLog.rows.length > 0) {
-        const names = briefingLog.rows.map((r) => r.full_name).join(', ');
+        const names = briefingLog.rows.map((r) => r.full_name).join('\n  • ');
         alerts.push(
-          `❗ Скоро истекает срок повторного инструктажа по ОТ: ${names}`
+          `❗ *Инструктаж по ОТ:*\n  • ${names}`
         );
       }
 
       if (fireBriefingLog.rows.length > 0) {
-        const names = fireBriefingLog.rows.map((r) => r.full_name).join(', ');
-        alerts.push(`🔥 Скоро истекает срок пожарного инструктаж: ${names}`);
+        const names = fireBriefingLog.rows.map((r) => r.full_name).join('\n  • ');
+        alerts.push(`🔥 *Пожарный инструктаж:*\n  • ${names}`);
       }
 
       if (powerToolLog.rows.length > 0) {
-        const names = powerToolLog.rows.map((r) => r.name).join(', ');
+        const names = powerToolLog.rows.map((r) => `${r.name} (инв. №${r.inventory_number})`).join('\n  • ');
         alerts.push(
-          `🔧 Скоро истекает срок осмотра электроинструмента: ${names}`
+          `🔧 *Осмотр электроинструмента:*\n  • ${names}`
         );
       }
 
       if (slingLog.rows.length > 0) {
-        const names = slingLog.rows.map((r) => r.name).join(', ');
-        alerts.push(`🪝 Скоро истекает срок осмотра строп: ${names}`);
+        const names = slingLog.rows.map((r) => `${r.name} (инв. №${r.inventory_number})`).join('\n  • ');
+        alerts.push(`🪝 *Осмотр строп:*\n  • ${names}`);
       }
 
       if (alerts.length === 0) {
@@ -51,10 +50,7 @@ export const startDailyCronJob = (bot) => {
         return;
       }
 
-      const messageText =
-        `🚨 *Напоминание о предстоящих проверках (менее 5 дней до окончания)*: ${alerts.join(
-          '\n\n'
-        )}`.trim();
+      const messageText =`🚨 *Напоминание о проверках (< 5 дней):*\n\n${alerts.join('\n\n')}`.trim();
       const chatIds = await client.query(`SELECT user_chat_id FROM auth`);
       for (const row of chatIds.rows) {
         try {
@@ -69,7 +65,7 @@ export const startDailyCronJob = (bot) => {
         }
       }
     } catch (err) {
-      console.error('❌ Ошибка в ежедневном крон-задании:', error);
+      console.error('❌ Ошибка в ежедневном крон-задании:', err);
     }
   });
 };
